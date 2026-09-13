bcc3fd42f5301a4a8ef3d031450f2e5c6eb14655
Manish <smanishh9@gmail.com>
2026-09-13 17:17:21 +0530

    first commit

---
+++ src/main/java/com/example/awsmysql/AwsmysqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.awsmysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsmysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsmysqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/awsmysql/AwsmysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.awsmysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsmysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
